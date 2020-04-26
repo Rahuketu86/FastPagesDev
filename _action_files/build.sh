b6c2878d731f0934c7ed1bbe5ab24196c3bfbdfc
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 23:26:00 +0530

    Modified emacs version and fixes to view post dir

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-add-apt-repository ppa:kelleyk/emacs
-apt-get update
-apt install emacs26
+apt install emacs-nox
 emacs --version
 cd ${GITHUB_WORKSPACE}
 emacs --batch --no-init-file --load ${GITHUB_WORKSPACE}/_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
