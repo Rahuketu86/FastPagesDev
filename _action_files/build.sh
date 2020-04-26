5a4c47076b89293d8144c17b8196c378fcf93ed1
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 23:07:09 +0530

    Modern emacs

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-apt-get install -y emacs
+add-apt-repository ppa:kelleyk/emacs
+apt-get update
+apt install emacs26
 emacs --version
 cd ${GITHUB_WORKSPACE}
 emacs --batch --no-init-file --load ${GITHUB_WORKSPACE}/_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
