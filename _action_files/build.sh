3a2dd444e21485c5ffff3a1420a97c785300efea
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 23:23:51 +0530

    Some more trials

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -6,3 +6,5 @@ apt install emacs26
 emacs --version
 cd ${GITHUB_WORKSPACE}
 emacs --batch --no-init-file --load ${GITHUB_WORKSPACE}/_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
+
+ls -la ${GITHUB_WORKSPACE}/_posts
