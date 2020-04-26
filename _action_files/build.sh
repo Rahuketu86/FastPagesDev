f2ce3ce1c10aadd0dc955b5c507818fe4c45e7f8
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 22:02:13 +0530

    Removed Sudo

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-sudo apt-get install -y emacs
+
+apt-get install -y emacs
+emacs --version
 emacs --batch --no-init-file --load ./_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
