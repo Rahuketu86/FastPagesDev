dfd3d2757a03058f972407b2844351f0b1fc9d59
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 19:42:59 +0530

    Updated publish.el to check variation

--- _action_files/publish.el
+++ _action_files/publish.el
@@ -1,12 +1,12 @@
 (require 'package)
 (package-initialize)
-(unless package-archive-contents
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (package-refresh-contents))
-(dolist (pkg '(org-plus-contrib htmlize))
-  (unless (package-installed-p pkg)
-    (package-install pkg)))
+;;(unless package-archive-contents
+;;  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+;;  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;  (package-refresh-contents))
+;;(dolist (pkg '(org-plus-contrib htmlize))
+;;  (unless (package-installed-p pkg)
+;;    (package-install pkg)))
 
 (require 'org)
 (require 'ox-rss)
