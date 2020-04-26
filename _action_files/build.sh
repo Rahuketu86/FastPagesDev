be2b578acdf633cb28b49575e13b009ce68b8c6e
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 20:22:10 +0530

    Fixed path in build.sh

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-emacs --batch --no-init-file --load publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
+emacs --batch --no-init-file --load ./_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
