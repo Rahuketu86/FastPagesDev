93685ac3a1c4dff3dcde1841a308189db5db4df4
Rahuketu86 <rahuketu86@gmail.com>
2020-04-26 23:28:59 +0530

    Added minor modification to install emacs

--- _action_files/build.sh
+++ _action_files/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install emacs-nox
+apt-get install -y emacs-nox
 emacs --version
 cd ${GITHUB_WORKSPACE}
 emacs --batch --no-init-file --load ${GITHUB_WORKSPACE}/_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
