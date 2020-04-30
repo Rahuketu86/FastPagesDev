23e6402e963124b846fbfa6e71d6adcc8da82851
Rahul Saraf <rahul.saraf@halliburton.com>
2020-04-30 12:27:33 +0000

    Fixed link library in init file

--- _custom_action_files/custom.sh
+++ _custom_action_files/custom.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Inside custom converter code"
-emacs --batch --no-init-file --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
+# emacs --batch --no-init-file --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
+emacs --batch --load ~/.emacs.d/init.el --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
