022ef92dc07ad731c22b7dbb65155432e56470f6
Rahul Saraf <rahul.saraf@halliburton.com>
2020-04-30 12:40:27 +0000

    Work around for supplyting init file automatically without ~

--- _custom_action_files/custom.sh
+++ _custom_action_files/custom.sh
@@ -2,4 +2,4 @@
 
 echo "Inside custom converter code"
 # emacs --batch --no-init-file --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
-emacs --batch --load ~/.emacs.d/init.el --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
+emacs -u "$(id -un)"  --batch --eval '(load user-init-file)' --load ./_custom_action_files/publish.el --funcall toggle-debug-on-error --funcall fastpages-publish-all
